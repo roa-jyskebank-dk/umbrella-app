c18bde917a827e2a43ef471a39ecaf9e3404d505
Rasmus Holm Andersen <roa@jyskebank.dk>
2021-03-03 21:58:35 +0100

    Fixed ng test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let httpClient: HttpClient = jasmine.createSpyObj('HttpClient', ['get']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpClient,
+        },
+      ],
     }).compileComponents();
   });
 
@@ -18,8 +27,10 @@ describe('AppComponent', () => {
   it('should render greeting', () => {
     // Arrange
     const response = { fact: 'A man once ate a bicycle' };
+    (httpClient.get as jasmine.Spy).and.returnValue(of(response));
+
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.componentInstance.response$ = of(response);
+
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
 
